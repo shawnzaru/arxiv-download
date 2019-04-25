da08bc3283f11f96d6c0733b2239c5cd34568dbf
Qingxiang Wang (Shawn) <shawn.wangqingxiang@gmail.com>
2019-04-25 19:10:04 +0200

    list_objects -> list_objects_v2

--- download.py
+++ download.py
@@ -4,7 +4,7 @@ import config
 # create session, resource and bucket
 session = boto3.session.Session(profile_name=config.ArXivConfig.PROFILE_NAME)
 client = session.client('s3')
-paginator = client.get_paginator('list_objects')
+paginator = client.get_paginator('list_objects_v2')
 
 page_iterator = paginator.paginate(Bucket='arxiv', Prefix='src', RequestPayer='requester')
 
