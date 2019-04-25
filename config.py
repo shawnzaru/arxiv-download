707f4e5954f4bf5ede9adc635e607c2f25522c7f
Qingxiang Wang (Shawn) <shawn.wangqingxiang@gmail.com>
2019-04-25 16:08:54 +0200

    add config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class ArXivConfig(object):
+  ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+  DATA_DIR = ROOT_DIR + '/data'
+  DOWNLOAD_DIR = DATA_DIR + '/downloads'
+  SRC_DIR = DOWNLOAD_DIR + '/src'
+  PDF_DIR = DOWNLOAD_DIR + '/pdf'
+  MAIN_FILE_DIR = DATA_DIR + '/main-files'
