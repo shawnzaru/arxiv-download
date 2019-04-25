c75d3b5c4678348a2c34a43fd286149720bf7d8f
Qingxiang Wang (Shawn) <shawn.wangqingxiang@gmail.com>
2019-04-25 16:20:33 +0200

    add META_DATA_DIR

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ class ArXivConfig(object):
   DOWNLOAD_DIR = DATA_DIR + '/downloads'
   SRC_DIR = DOWNLOAD_DIR + '/src'
   PDF_DIR = DOWNLOAD_DIR + '/pdf'
+  META_DATA_DIR	= DATA_DIR + '/meta-data'
   MAIN_FILE_DIR = DATA_DIR + '/main-files'
